74a94a21737eeb27d521289bb85c9c6876a87c7c
Logan <milerl1960@gmail.com>
2023-02-24 18:37:31 -0800

    Stuck joining data with tract outlines into folium map

--- NacaMap/DataExplorer.py
+++ NacaMap/DataExplorer.py
@@ -1,24 +1,30 @@
+import geopandas as gpd
 import pandas as pd
 
-# Replace file_path with the actual path to your CSV file
-file_path = r"C:\Users\mille\PycharmProjects\naca-react-django-app\NacaMap\data\Income\ACSST5Y2021.S1903-Data.csv"
+# Replace shapefile_path with the actual path to your shapefile
+shapefile_path = r"C:\Users\mille\PycharmProjects\naca-react-django-app\NacaMap\data\CensusTracts\Oregon\tl_2022_41_tract.shp"
 
-# Read the CSV file into a pandas dataframe
-df = pd.read_csv(file_path)
+# Read the shapefile into a geopandas dataframe
+gdf = gpd.read_file(shapefile_path)
 
-# Find the column that contains "median income" in its title
-median_income_col = None
-for col in df.columns:
-    print(col)
-    if "median" in col.lower():
-        median_income_col = col
-        break
+# Replace income_data_path with the actual path to your income data CSV file
+income_data_path = r"C:\Users\mille\PycharmProjects\naca-react-django-app\NacaMap\data\Income\ACSST5Y2021.S1903-Data_new.csv"
 
-# Check if a column containing "median income" was found
-if median_income_col is not None:
-    # Select the column containing "median income" into a new dataframe
-    median_income_df = df[[median_income_col]]
-    print("Selected column:", median_income_col)
-    print(median_income_df.head())
-else:
-    print("No column containing 'median income' was found.")
+# Read the income data CSV file into a pandas dataframe
+income_df = pd.read_csv(income_data_path)
+income_df = income_df.rename(columns={"GEO_ID": "GEOID"})
+
+# Join the shapefile and the income data by the "GEOID" column
+joined_df = gdf.merge(income_df, on="GEOID")
+
+# Make sure that both datasets have the same coordinate reference system (CRS)
+# In this example, we assume that the CRS of the shapefile is EPSG:4326
+# and we set the CRS of the joined dataframe to EPSG:4326 as well
+joined_df = joined_df.to_crs(epsg=4326)
+
+file_path=r"C:\Users\mille\PycharmProjects\naca-react-django-app\NacaMap\data\joined_data.csv"
+
+# Save the new dataset as a CSV to the same folder path as the original CSV
+joined_df.to_csv(file_path, index=False)
+
+print("New CSV saved to:", file_path)
--- NacaMap/NacaMap/views.py
+++ NacaMap/NacaMap/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import folium
+import pandas as pd
 from folium import Element
 
 def update_map(request):
@@ -13,7 +14,32 @@ def update_map(request):
   return ""
 
 def map_view(request):
+    # Replace joined_data_path with the actual path to your joined dataset
+    joined_data_path = r"C:\Users\mille\PycharmProjects\naca-react-django-app\NacaMap\data\Income\ACSST5Y2021.S1903-Data_new.csv"
+
+    # Read the joined dataset into a pandas dataframe
+    joined_df = pd.read_csv(joined_data_path)
+
+    joined_df.dropna()
+
+    joined_df = joined_df.loc[~(joined_df['MedianIncome'].isna())]
+
     map = folium.Map(location=[45.5236, -122.6750], zoom_start=13)
+
+    # Add the joined dataset as a choropleth layer
+    folium.Choropleth(
+        geo_data=joined_df,
+        data=joined_df,
+        columns=["GEO_ID", "MedianIncome"],
+        key_on="feature.properties.GEOID",
+        fill_color="YlOrRd",
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name="Median Income",
+        highlight=True,
+        name="Median Income",
+        overlay=True
+    ).add_to(map)
 
     map = map._repr_html_() # render map as HTML
     return render(request, 'map.html', {'map': map})
